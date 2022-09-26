b08ef79d8cc4cf6287a21ef09c8915129aadcae2
Xila <ctreinauer@gmail.com>
2022-09-26 01:08:27 -0600

    Update styling and placement

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
+//reset button declaration and event listener//
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener('click', shake);
 
 
-
-
-
+document.getElementById('sketchpad').addEventListener('mouseover', function(e) {
+    e.target.classList.add('colorDiv'); //add class list to divs that are hovered - for styling//
+})
 //declare sketchpad// 
- let sketchpad = document.getElementById('sketchpad');
+const sketchpad = document.getElementById('sketchpad');
 
 // declare slider variables for Event Listener // 
-   let slider = document.getElementById("range");
-   let output = document.getElementById("value");
-  
+let slider = document.getElementById("range");
+let output = document.getElementById("value");
+
 // add Event Listener and store info in variable for grid size //
-  slider.addEventListener('mouseup', function () {
+slider.addEventListener('mouseup', function() {
     let gridSize = slider.value;
-    console.log(gridSize); 
- 
+    console.log(gridSize);
+
     createGrid(gridSize); //initiate function on slider change// 
 
     function createGrid() {
-      let rows = gridSize; 
-      let finalGridSize = rows ** 2; //determine grid size//
-    for (let i = 0; i < finalGridSize; i++) {
-      let gridItem = document.createElement('div'); //create a class for the divs// 
-      sketchpad.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; //sizing and styling of basic grid//
-      sketchpad.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;    //sizing and styling of basic grid//
-      sketchpad.append(gridItem); //add grid to sketchpad div//
-
-      document.getElementById('sketchpad').addEventListener('mouseover', function (e) {
-        e.target.classList.add('colorDiv');  //add class list to divs that are hovered - for styling//
-      })
-
-  }
-}
+        let rows = gridSize;
+        let finalGridSize = rows ** 2; //determine grid size//
+        for (let i = 0; i < finalGridSize; i++) {
+            const gridItem = document.createElement('div'); //create the divs// 
+            gridItem.className = 'sketchpad-div' //create a class for the newly created divs//
+            sketchpad.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; //sizing and styling of basic grid//
+            sketchpad.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`; //sizing and styling of basic grid//
+            sketchpad.append(gridItem); //add grid to sketchpad div//
+        }
+    }
 })
 
-
+function shake() {
+    const gridItems = document.querySelectorAll('.sketchpad-div')
+    gridItems.forEach((item) => {
+        item.className = 'sketchpad-div'; //reset class name to reset sketchpad// 
+    })
+}
